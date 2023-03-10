2a6df6ef41d2b1c5041a1545d48d51e06d4ba5dd
Telemaco Terzi <tezze.terzi01@google.com>
2023-03-10 10:03:21 +0100

    finale-test

--- image_datasets/README.md
+++ image_datasets/README.md
@@ -1,1 +1,2 @@
-wdwd
+# Datasets
+[yolo datataset]()
--- source/__main__.py
+++ source/__main__.py
@@ -1,19 +1,25 @@
 from rembg import remove, new_session
 import PIL
 
-if __name__ == "__main__":
-    
+def separate_bg_fg(input: PIL.Image):
     session = new_session()
-    
-    input_path = "./source/tests/input.jpg"
-    
-    input = PIL.Image.open(input_path)
+
     input_copy = input.copy()
 
     alpha_foreground = remove(input, session=session, only_mask=True, post_process_mask=True)
     alpha_background = PIL.ImageChops.invert(alpha_foreground)
 
     input.putalpha(alpha_foreground)
-    input.save("./source/tests/foreground.png")
-    input.putalpha(alpha_background)
-    input.save("./source/tests/background.png")
+    input_copy.putalpha(alpha_background)
+    return (input_copy, input)
+
+if __name__ == "__main__":
+    
+    input_path = "./source/tests/input.jpg"
+    input = PIL.Image.open(input_path)
+    (bg, fg) = separate_bg_fg(input)
+    (bg1, fg1) = separate_bg_fg(fg)
+
+    bg.save('./source/tests/bg.png')
+    bg1.save('./source/tests/2-fg.png')
+    fg1.save('./source/tests/fg.png')
